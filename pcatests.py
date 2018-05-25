e9d40b2181310ef72c57b594c298799f94e3e6fe
Iman <25533623+whatsthedhillo@users.noreply.github.com>
2018-05-25 06:00:27 -0400

    Add files via upload

---
+++ pcatests.py
@@ -0,0 +1,90 @@
+import numpy as np
+from sklearn.decomposition import PCA, KernelPCA
+import matplotlib.pyplot as plt
+from matplotlib import pylab
+
+
+'''
+import itertools as itt
+from collections import Counter
+n = 3
+xy = ("x", "y") # list of variables may be extended indefinitely
+poly = '+'.join(itt.starmap(lambda u, t: u+"*"+t if t else u, zip(map(lambda v: "C["+str(v)+"]", itt.count()),map(lambda z: "*".join(z), map(lambda x: tuple(map(lambda y: "**".join(map(str, filter(lambda w: w!=1, y))), x)), map(dict.items, (map(Counter, itt.chain.from_iterable(itt.combinations_with_replacement(xy, i) for i in range(n+1))))))))))
+'''
+
+
+# Generate Gaussian datasets (2/high dimensional)
+mean = (3,3)
+cov = [[2,0],[0,2]]
+gauss2d = np.random.multivariate_normal(mean, cov, 1000)
+
+meanhighD = np.random.randint(10, size=50)
+covhighD = np.identity(50, dtype=int)
+gausshighD = np.random.multivariate_normal(meanhighD, covhighD, 1000)
+
+##################
+# Generate non-Gaussian datasets (2/high dimensional)
+points = np.array([(1, 1), (2, 4), (3, 1), (9, 3)])
+x = points[:,0]
+y = points[:,1]
+
+z = np.polyfit(x, y, 3)
+f = np.poly1d(z)
+
+x_new = np.linspace(x[0], x[-1], 50)
+y_new = f(x_new)
+polynom2d = np.vstack((x_new, y_new))
+
+#plt.plot(x,y,'o', x_new, y_new)
+#plt.show()
+
+# Now add noise
+
+
+# Generate high dim signal
+
+sin = np.zeros((50, 1000))
+for i in range(50):
+    # dec[0] is for (high/low) amplitude
+    # dec[1] is for (high/low) noise variance
+    # dec[2] is for (high/low) frequency
+    # where < means low and >= means high
+    dec = np.random.uniform(-1, 1, size=3)
+
+    a = np.arange(1000, dtype=float) #.reshape((1000/50),50)
+
+    if dec[0] < 0.3:
+        amp = np.random.normal(0.5, 1.3)
+    elif dec[0] >= 0.3:
+        amp = np.random.normal(1.2, 2.0)
+
+    if dec[1] < 0:
+        noise = np.random.normal(-0.4, 0.3, size=1000)
+    elif dec[1] >= 0:
+        noise = np.random.normal(0.4, 1.1, size=1000)
+
+    if dec[2] < 0.45:
+        radinterval = np.random.normal(0.5, 1.3)
+    elif dec[2] >= 0.45:
+        radinterval = np.random.normal(1.3, 2.1)
+    
+    a *= radinterval
+    rad = np.radians(a)
+
+    sin[i] = (amp * np.sin(rad)) + noise
+
+    # Will make label vector soon
+
+
+# Run PCA on data
+pca = PCA(n_components=9)
+pca.fit(sin)
+print('Explained')
+print(pca.explained_variance_ratio_)
+print('Singular Values')
+print(pca.singular_values_) 
+
+kpca = KernelPCA(n_components=9)
+kpca.fit(sin)
+print('kpca lambdas')
+print(kpca.lambdas_)
